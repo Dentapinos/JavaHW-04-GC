ec5103dae080623eeb425154fd283bf1925c2943
DenisLadchenkov <119222041+Dentapinos@users.noreply.github.com>
2024-12-06 15:29:09 +0300

    hw04 garbage collector

---
+++ Data.java
@@ -0,0 +1,12 @@
+
+public class Data {
+    private final int value;
+
+    public Data(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+}
